a45ec9a5041df07237ae4fe1135416eba1720929
Asia sama <sehunexo710@gmail.com>
2024-03-07 21:22:54 +0300

    remove import

--- app/src/main/java/com/hub/wifianalysis/ui/MainActivity.kt
+++ app/src/main/java/com/hub/wifianalysis/ui/MainActivity.kt
@@ -2,18 +2,10 @@ package com.hub.wifianalysis.ui
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
 import com.hub.wifianalysis.R
 import com.hub.wifianalysis.databinding.ActivityMainBinding
-import tej.androidnetworktools.lib.Device
-import tej.androidnetworktools.lib.scanner.NetworkScanner
-import tej.androidnetworktools.lib.scanner.OnNetworkScanListener
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
